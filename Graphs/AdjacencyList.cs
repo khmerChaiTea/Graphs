c890ffcc28164c90345bc6858e7b8c1aeed88240
pchai <prempreychhay@gmail.com>
2024-08-15 15:39:13 -0700

    Added Edge

--- Graphs/AdjacencyList.cs
+++ Graphs/AdjacencyList.cs
@@ -3,11 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Graphs
 {
     public class AdjacencyList
     {
-        public Dictionary<int, List<int>> list;
+        public Dictionary<int, LinkedList<int>> list = new();
+
+        public AdjacencyList(int verticesSize)
+        {
+            for (int i = 0; i < verticesSize; i++)
+            {
+                list.Add(i, new());
+            }
+        }
+
+        public void AddEdge(int src, int dest)
+        {
+            if (list.TryGetValue(src, out LinkedList<int>? value))
+            {
+                value.AddFirst(dest);
+            }
+            else
+            {
+                // Initialize the LinkedList if it doesn't exist
+                value = new LinkedList<int>();
+                value.AddFirst(dest);
+                list[src] = value;
+            }
+
+            // Assuming undirected graph, add the reverse edge as well
+            if (list.TryGetValue(dest, out LinkedList<int>? destValue))
+            {
+                destValue.AddFirst(src);
+            }
+            else
+            {
+                // Initialize the LinkedList if it doesn't exist
+                destValue = new LinkedList<int>();
+                destValue.AddFirst(src);
+                list[dest] = destValue;
+            }
+        }
+
+        public bool ContainsEdge(int src, int dest)
+        {
+            LinkedList<int>? value;
+            list.TryGetValue(src, out value);
+            if (value == null) return false;
+            return value.Contains(dest);
+        }
     }
 }
--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AdjacencyList list = new AdjacencyList(10);
+            list.AddEdge(0, 1);
+            list.AddEdge(0, 2);
+            list.AddEdge(1, 2);
+            list.AddEdge(1, 3);
+            list.AddEdge(2, 4);
+            list.AddEdge(3, 4);
+            list.AddEdge(4, 5);
+            list.AddEdge(5, 6);
+            list.AddEdge(7, 9);
+            list.AddEdge(8, 9);
+            Console.WriteLine(list.ContainsEdge(1, 3));
+            Console.WriteLine(list.ContainsEdge(0, 9));
         }
     }
 }
